abb70a4c310ce304e8478466ffeb6455c7ca250c
AlexGuillemin91 <alex.guillemin@edu.devinci.fr>
2022-12-10 00:13:17 +0100

    reajust test and divide it in 2 describe

--- account.spec.ts
+++ account.spec.ts
@@ -1,6 +1,6 @@
 import { Account } from './account';
 
-describe('account', () => {
+describe('account basic actions', () => {
     let account1: Account
     beforeEach(() => {
         account1 = new Account(5000)
@@ -26,6 +26,10 @@ describe('account', () => {
     it('should return the history of transactions empty', () => {
         expect(account1.getHistory()).toStrictEqual([])
     })
+    it('should return the history of transactions', () => {
+        account1.history = ['300 withdrawn', '300 deposed']
+        expect(account1.getHistory()).toStrictEqual(['300 withdrawn', '300 deposed'])
+    })
     it('should return the history of transactions with withdraw and deposit', () => {
         account1.withdrawal(300)
         account1.deposit(300)
@@ -36,18 +40,43 @@ describe('account', () => {
         account1.deposit(500)
         expect(account1.getHistory()).toStrictEqual(['500 withdrawn', '500 deposed'])
     })
-    it('should return the history of transactions', () => {
-        account1.history = ['300 withdrawn', '300 deposed']
-        expect(account1.getHistory()).toStrictEqual(['300 withdrawn', '300 deposed'])
+})
+describe('account transfers', () => {
+    let account1: Account
+    let account2: Account
+    beforeEach(() => {
+        account1 = new Account(5000)
+        account2 = new Account(2500)
+    })
+    it('should update the first account balance', () => {
+        expect(account1.transfer(account2, 700)).toBe(4300)
+    })
+    it('should update the second account balance', () => {
+        account1.transfer(account2, 600)
+        expect(account2.balance).toBe(3100)
     })
     it('should update the 2 balance', () => {
-        const account2 = new Account(2500)
         expect(account1.transfer(account2, 500)).toBe(4500)
         expect(account2.balance).toBe(3000)
     })
     it('should update the 2 balance', () => {
-        const account2 = new Account(2500)
         expect(account1.transfer(account2, 1000)).toBe(4000)
         expect(account2.balance).toBe(3500)
+    })
+    it('should return the first account history', () => {
+        account1.transfer(account2, 500)
+        expect(account1.history).toStrictEqual(["500 transfered"])
+    })
+    it('should return the first account history', () => {
+        account1.transfer(account2, 600)
+        expect(account1.history).toStrictEqual(["600 transfered"])
+    })
+    it('should return the second account history', () => {
+        account1.transfer(account2, 400)
+        expect(account2.history).toStrictEqual(["400 received"])
+    })
+    it('should return the second account history', () => {
+        account1.transfer(account2, 700)
+        expect(account2.history).toStrictEqual(["700 received"])
     })
 })
